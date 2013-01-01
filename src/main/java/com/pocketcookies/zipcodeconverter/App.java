092ac0e014ad7c845f773ebb7ff3fba4ba7cd689
John Edmonds <john.a.edmonds@gmail.com>
2013-01-01 15:46:12 -0500

    Change main to getZipCodes() which can do a batch conversion from coordinates to zip codes.

--- src/main/java/com/pocketcookies/zipcodeconverter/App.java
+++ src/main/java/com/pocketcookies/zipcodeconverter/App.java
@@ -1,16 +1,17 @@
 package com.pocketcookies.zipcodeconverter;
 
+import com.google.common.collect.ImmutableMap;
+import com.sun.istack.internal.Nullable;
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.MultiPolygon;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Properties;
 import org.geotools.data.shapefile.ShapefileDataStore;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.opengis.feature.simple.SimpleFeature;
-import com.vividsolutions.jts.geom.MultiPolygon;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.Coordinate;
-import java.io.BufferedReader;
 
 /**
  * Hello world!
@@ -25,29 +26,58 @@ public class App {
     private static String getShapefilePath(String id) {
         return "/zipcodes/" + getShapefileName(id);
     }
+    private static final Properties shapefiles = new Properties();
 
-    public static void main(String[] args) throws IOException {
-        Properties shapefiles = new Properties();
-        shapefiles.load(App.class.getResourceAsStream("/zipcodes/shapefiles.txt"));
+    static {
+        try {
+            shapefiles.load(App.class.getResourceAsStream("/zipcodes/shapefiles.txt"));
+        } catch (IOException ex) {
+            throw new ExceptionInInitializerError("Failed to load the list of shapefiles.");
+        }
+    }
 
-        BufferedReader systemIn = new BufferedReader(new InputStreamReader(System.in));
-        String line;
-        while ((line = systemIn.readLine()) != null) {
-            double x = Double.parseDouble(line.split("[, ]")[0]);
-            double y = Double.parseDouble(line.split("[, ]")[1]);
-            for (Entry<Object, Object> shapefileEntry : shapefiles.entrySet()) {
-                ShapefileDataStore shapefile = new ShapefileDataStore(App.class.getResource(getShapefilePath((String) shapefileEntry.getValue())));
-                // Check the bounds of this entire shapefile to see if we can skip it.
-                if (shapefile.getFeatureSource().getBounds().contains(x, y)) {
-                    SimpleFeatureIterator featureIterator = shapefile.getFeatureSource().getFeatures().features();
+    /**
+     * Figures out which zip codes contain the given {@link Coordinates}.
+     *
+     * @param coordinates The coordinates for which to find containing zip
+     * codes.
+     * @return A map from the coordinates to the zip code (e.g. 10011)
+     * containing that coordinate. If no zip code contains that coordinate, it
+     * will not be in the map.
+     * @throws IOException If there's a problem loading or reading from the
+     * shapefile. In practice, this should be impossible since the shapefiles
+     * should be packaged in the jar.
+     */
+    @Nullable
+    public static Map<Coordinate, String> getZipCodes(Iterable<Coordinate> coordinates)
+            throws IOException {
+        final ImmutableMap.Builder<Coordinate, String> zipCodes = ImmutableMap.builder();
+        for (Entry<Object, Object> shapefileEntry : shapefiles.entrySet()) {
+            ShapefileDataStore shapefile = new ShapefileDataStore(
+                    App.class.getResource(
+                    getShapefilePath((String) shapefileEntry.getValue())));
+            for (Coordinate coordinate : coordinates) {
+                // Check the bounds of this entire shapefile to see if we can
+                // skip it.
+                if (shapefile.getFeatureSource().getBounds()
+                        .contains(coordinate)) {
+                    SimpleFeatureIterator featureIterator = shapefile
+                            .getFeatureSource()
+                            .getFeatures()
+                            .features();
                     while (featureIterator.hasNext()) {
                         SimpleFeature feature = featureIterator.next();
-                        if (((MultiPolygon) feature.getDefaultGeometry()).contains(new GeometryFactory().createPoint(new Coordinate(x, y)))) {
-                            System.out.println("Found in " + feature.getAttribute("NAME"));
+                        if (((MultiPolygon) feature.getDefaultGeometry())
+                                .contains(
+                                new GeometryFactory()
+                                .createPoint(coordinate))) {
+                            zipCodes.put(coordinate, (String) feature.getAttribute("NAME"));
+                            break;
                         }
                     }
                 }
             }
         }
+        return zipCodes.build();
     }
 }
