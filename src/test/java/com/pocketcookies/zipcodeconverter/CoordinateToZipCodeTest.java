25389a3e78149855c040f058de294dda54981d1f
John Edmonds <john.a.edmonds@gmail.com>
2013-01-21 22:56:15 -0500

    Add more tests.

--- src/test/java/com/pocketcookies/zipcodeconverter/CoordinateToZipCodeTest.java
+++ src/test/java/com/pocketcookies/zipcodeconverter/CoordinateToZipCodeTest.java
@@ -5,6 +5,7 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Iterables;
 import com.vividsolutions.jts.geom.Coordinate;
+import java.io.IOException;
 import junit.framework.TestCase;
 
 /**
@@ -15,13 +16,23 @@ import junit.framework.TestCase;
 public class CoordinateToZipCodeTest extends TestCase {
 
     public void testZipCode() throws Exception {
-        assertEquals("20815", Iterables.getOnlyElement(
-                CoordinateToZipCode.getZipCodes(
-                ImmutableSet.of(new Coordinate(-77.06720, 39.00404)))
-                .entrySet()).getValue());
+        assertZipCode("20815", new Coordinate(-77.06720, 39.00404));
+        assertZipCode("20032", new Coordinate(-76.9880718769312, 38.8324247044461));
+        assertZipCode("20812", new Coordinate(-77.1435801669312, 38.9699880244461));
+        assertEquals(
+                ImmutableSet.of("20815", "20032", "20812"),
+                ImmutableSet.copyOf(
+                CoordinateToZipCode.getZipCodes(ImmutableSet.of(
+                new Coordinate(-77.1435801669312, 38.9699880244461),
+                new Coordinate(-76.9880718769312, 38.8324247044461),
+                new Coordinate(-77.06720, 39.00404))).values()));
     }
     
     public void testMissingZipCode() throws Exception {
         assertEquals(ImmutableMap.of(), CoordinateToZipCode.getZipCodes(ImmutableSet.of(new Coordinate(37.71859, -39.023437))));
+    }
+    
+    private static void assertZipCode(String zipCode, Coordinate coordinate) throws IOException {
+        assertEquals(zipCode, Iterables.getOnlyElement(CoordinateToZipCode.getZipCodes(ImmutableSet.of(coordinate)).entrySet()).getValue());
     }
 }
